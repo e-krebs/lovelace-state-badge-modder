973742fafad05595e65edb8c31f587551ee2f2bd
e-krebs <e-krebs@users.noreply.github.com>
2020-07-19 17:13:53 +0200

    update to work with card tools v2

--- README.md
+++ README.md
@@ -4,28 +4,21 @@ Allows you to style home-assistant lovelace state-badges
 
 This card requires [card-tools](https://github.com/thomasloven/lovelace-card-tools) to be installed.
 
-For installation instructions [see this guide](https://github.com/thomasloven/hass-config/wiki/Lovelace-Plugins).
-
-It is inspired by Thomas Lovén's [lovelace-card-modder](https://github.com/thomasloven/lovelace-card-modder).
-
 ## Options
 
 | Name | Type | Default | Description
 | ---- | ---- | ------- | -----------
 | type | string | **Required** | `custom:state-badge-modder`
-| card | object | **Required** | The card you wish to style
-| thing | string | element | `card` or `element` Wether the root element that contains the state-card is an element (ex: `picture-elements`) or a card (ex: `glance`)
+| card | object | **Required** | The card you wish to style **(it is expected to be type: state-icon)**
 | style | list | none | List of css styles to apply to card
 | state_badge_css | string | none | A style sheet url to add to the state-badge element(s)
-| state_badge_styles | string | none | Extra style data to add to the state-badge element(s)
+| extra_css | string | none | Another style sheet url to add to the state-badge element(s)
 
 # Styling
 
-state-badge-modder can be used to apply CSS styling to any lovelace card.
+state-badge-modder can be used to apply CSS styling on a **state-icon** card.
 
 it will add thoses styles to the inner `state-badge` element(s).
-
-(tested with `picture-elements` and `glance`)
 
 Any CSS style can be used, and will be applied to all the `state-badge` element(s) of the card
 (`<state-badge>`). Most cards use css variables for styling, and to find out which
@@ -35,49 +28,15 @@ or that you open the card in your browsers object inspector and check out the
 styling options manually.
 
 ```yaml
-- type: picture-elements
-  image: /local/floorplan/floorplan-map.svg
-  elements:
-    - type: custom:state-badge-modder
-      card:
-        type: state-icon
-        entity: media_player.pc_universal
-        hold_action:
-          action: toggle
-      style:
-        bottom: 5%
-        left: 8.6%
-      state_badge_css: /local/custom-ui/state-badge.css?v=0.0.8
-      state_badge_styles: >
-        ha-icon[data-domain="media_player"][data-state="on"] {
-          background-image: url('/local/entity/windows-10.jpg');
-          color: transparent;
-          border-radius: 50%;
-          background-size: contain;
-        }
-
-- type: custom:state-badge-modder
-  thing: card
-  card:
-    type: glance
-    title: roupille
-    show_name: false
-    entities:
-      - entity: sensor.roupille_krebs_tech
-        icon: mdi:ip-network
-      - sensor.roupille_expiry
-      - entity: media_player.pc_universal
-        name: pc
-        hold_action:
-          action: toggle
-      - entity: sensor.pc_power
-        name: ''
-  state_badge_css: /local/custom-ui/state-badge.css?v=0.0.8
-  state_badge_styles: >
-    ha-icon[data-domain="media_player"][data-state="on"] {
-      background-image: url('/local/entity/windows-10.jpg');
-      color: transparent;
-      border-radius: 50%;
-      background-size: contain;
-    }
+type: custom:state-badge-modder
+card:
+  type: state-icon
+  entity: media_player.pc_universal
+  hold_action:
+    action: toggle
+style:
+  bottom: 35.5%
+  left: 40%
+state_badge_css: /local/custom-ui/state-badge.css?v=0.0.14
+extra_css: /local/custom-ui/pc-universal.css?v=0.0.1
 ```
--- state-badge-modder.js
+++ state-badge-modder.js
@@ -1,166 +1,83 @@
 customElements.whenDefined('card-tools').then(() => {
-  class StateBadgeModder extends cardTools.litElement() {
+  var cardTools = customElements.get('card-tools');
 
-    constructor() {
-      super();
-      this.EL_STYLES = ["left", "top", "right", "bottom", "position"];
+  class StateBadgeModder2 extends cardTools.LitElement {
+
+    setConfig(config) {
+      if (config.extra_css) this.extra_css = config.extra_css;
+      if (config.state_badge_css) this.state_badge_css = config.state_badge_css;
+
+      this.cardConfig = config.card;
+      const card = cardTools.createElement(this.cardConfig);
+      cardTools.provideHass(card);
+      this.card = card;
+
+      this.cardMod();
     }
 
-    static get properties() {
-      return {
-        card: {},
-      };
-    }
-
-    async setConfig(config) {
-      cardTools.checkVersion(0.3);
-
-      if(!config || !config.card) {
-        throw new Error("Card config incorrect");
-      }
-      if(Array.isArray(config.card)) {
-        throw new Error("It says 'card', not 'cardS'. Remove the dash.");
-      }
-
-      this.templated = [];
-      this.attempts = 5;
-
-      if (config.entities)
-        config.card.entities = config.entities;
-
-      this.card = (config.thing && config.thing === 'card')
-        ? cardTools.createCard(config.card)
-        : cardTools.createElement(config.card);
-
-      if(this._hass)
-        this.card.hass = this._hass;
-
-      if(this._config)
-        this._cardMod();
-
-      this._config = config;
-    }
-
-    createRenderRoot() {
-      return this;
-    }
-    render() {
-      return cardTools.litHtml()`
-    <div id="root">${this.card}</div>
-    `;
-    }
-
-    async firstUpdated() {
-      this._cardMod();
-    }
-
-    async _cardMod() {
+    async cardMod() {
       let root = this.card;
-      let targets = null;
-      let styles = null;
-      if(this.classList.contains("element")) {
-        // target = this.card;
-        root = this;
-      }
-      while(!targets) {
+      let badge = null;
+
+      while (!badge) {
         await root.updateComplete;
-        if(root.querySelector("style"))
+        if (root.querySelector("style"))
           styles = root.querySelector("style");
-        if(root.querySelector("state-badge")) {
-          targets = root.querySelectorAll("state-badge");
+        if (root.querySelector("state-badge")) {
+          badge = root.querySelector("state-badge");
           continue;
         }
-        if(root.card) {
+        if (root.card) {
           root = root.card;
           continue;
         }
-        if(root.shadowRoot) {
+        if (root.shadowRoot) {
           root = root.shadowRoot;
           continue;
         }
-        if(root.querySelector("#root")) {
+        if (root.querySelector("#root")) {
           root = root.querySelector("#root");
           continue;
         }
-        if(root.firstElementChild) {
+        if (root.firstElementChild) {
           root = root.firstElementChild;
           continue;
         }
         break;
       }
 
-      if(!targets && this.attempts) // Try again
-        setTimeout(() => this._cardMod(), 100);
-      this.attempts--;
-      targets = targets || [].concat(this.card);
-
-      targets.forEach((target) => {
-        let state_badge_styles = target.shadowRoot.querySelector("style");
-
-        if (this._config.state_badge_css) {
-          const link = document.createElement('link');
-          link.setAttribute('rel', 'stylesheet');
-          link.setAttribute('href', this._config.state_badge_css);
-          if (!target.shadowRoot.innerHTML.includes(this._config.state_badge_css)) {
-            target.shadowRoot.appendChild(link);
-          }
+      if (this.extra_css) {
+        const link = document.createElement('link');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', this.extra_css);
+        if (!badge.shadowRoot.innerHTML.includes(this.extra_css)) {
+          badge.shadowRoot.appendChild(link);
         }
+      }
 
-        if (this._config.state_badge_styles) {
-          if (!state_badge_styles) {
-            state_badge_styles = document.createElement('style');
-            root.appendChild(state_badge_styles);
-          }
-          if (!state_badge_styles.innerHTML.includes(this._config.state_badge_styles)) {
-            state_badge_styles.innerHTML += this._config.state_badge_styles;
-          }
+      if (this.state_badge_css) {
+        const link = document.createElement('link');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', this.state_badge_css);
+        if (!badge.shadowRoot.innerHTML.includes(this.state_badge_css)) {
+          badge.shadowRoot.appendChild(link);
         }
-
-        if(this._config.style) {
-          for(var k in this._config.style) {
-            if(cardTools.hasTemplate(this._config.style[k]))
-              this.templated.push(k);
-            if(this.card.style.setProperty)
-              this.card.style.setProperty(k, '');
-            if(target.style.setProperty) {
-              target.style.setProperty(k, cardTools.parseTemplate(this._config.style[k]));
-            }
-            if(this.classList.contains("element") && this.EL_STYLES.indexOf(k) > -1) {
-              this.style.setProperty(k, cardTools.parseTemplate(this._config.style[k]));
-            }
-          }
-        }
-      });
-      this.targets = targets;
+      }
     }
 
-    set hass(hass) {
-      this._hass = hass;
-      if(this.card) this.card.hass = hass;
-      if(this.templated)
-        this.templated.forEach((k) => {
-          this.targets.forEach((target) => {
-            target.style.setProperty(k, cardTools.parseTemplate(this._config.style[k], ''));
-            if(this.classList.contains("element") && this.EL_STYLES.indexOf(k) > -1) {
-              this.style.setProperty(k, cardTools.parseTemplate(this._config.style[k]));
-            }
-          });
-        });
-    }
-
-    getCardSize() {
-      if(this.card)
-        return typeof this.card.getCardSize === "function" ? this.card.getCardSize() : 1;
-      return 1;
+    render() {
+      return cardTools.LitHtml`
+      <div id="root">${this.card}</div>
+      `;
     }
   }
 
-  customElements.define('state-badge-modder', StateBadgeModder);
-});
+  customElements.define("state-badge-modder", StateBadgeModder2);
+}); // END OF .then(() => {
 
-window.setTimeout(() => {
-  if(customElements.get('card-tools')) return;
-  customElements.define('state-badge-modder', class extends HTMLElement{
-    setConfig() { throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools");}
+setTimeout(() => {
+  if (customElements.get('card-tools')) return;
+  customElements.define('state-badge-modder', class extends HTMLElement {
+    setConfig() { throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools"); }
   });
 }, 2000);
